4454a8113aa7c8a8407e0d0050a3d183285423d0
Mercedes Walsh <sadieswalsh@gmail.com>
2019-11-19 20:36:31 -0800

    added table

--- public/app.js
+++ public/app.js
@@ -1,3 +1,19 @@
+axios.get('/activites')
+.then(function(r) {
+    let newItem = document.createElement('tr')
+        newItem.innerHTML = `
+            <th>${item}</th>
+        `
+        document.getElementById('toDoList').append(newItem)
+        axios.put(`/activities/${e.target.id}`, {
+            finished: parseInt(e.target.dataset.finished) === 0 ? true : false
+        })
+        .then(() => {
+            window.location.reload()
+        })
+        .catch(e => console.error(e))
+})
+
 document.getElementById('addBtn').addEventListener('click', e => {
     e.preventDefault()
     let item = {
@@ -15,9 +31,9 @@ document.addEventListener('click', e => {
     if (e.target.className === 'item') {
         console.log(e.target.dataset.finished)
         console.log(e.target.id)
-        let newItem = document.createElement('ul')
+        let newItem = document.createElement('tr')
         newItem.innerHTML = `
-            <ul>${item}</ul>
+            <th>${item}</th>
         `
         document.getElementById('toDoList').append(newItem)
         axios.put(`/activities/${e.target.id}`, {
